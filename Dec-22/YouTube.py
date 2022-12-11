49e89ee8f3d6f892e110acd55723ced86734ce7a
BaileyLark <bailey.lark1@gmail.com>
2022-12-11 20:22:06 +0000

    0.02

--- Dec-22/YouTube.py
+++ Dec-22/YouTube.py
@@ -1,1 +1,12 @@
+from googleapiclient.discovery import build 
+
 api_key = 'AIzaSyBSVRfYPg0eVR-xrHhSswXMkjVIMai_SiA'
+youtube = build('youtube', 'v3', developerKey=api_key)
+request = youtube.channel().list(
+    part='statistics', forUsername='AuroraMusic')
+
+responce = request.execute()
+
+print(responce)
+
+
