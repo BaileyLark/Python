5622300172b39a9354aa5ed981e1078c7a4f9658
BaileyLark <bailey.lark1@gmail.com>
2023-01-01 20:02:19 +0000

    0.07

--- Dec-22/ML/NN.py
+++ Dec-22/ML/NN.py
@@ -1,4 +1,5 @@
 import numpy as np 
+np.random.seed(0)
 
 # shape 
 '''
@@ -12,13 +13,11 @@ l3 = [[[2, 3, 5, 4], # shape (3, 2, 4) or 3d array
      [[2, 3, 5, 6],
      [8, 5, 5, 4]]]
 '''
-
-
 # CALCULATIONS
 # (1 * 0.2) + (2 * 0.8) + (3 * -0.5) (1 * 1) + 2  is equal to neuron output 1
 # the dot product adds two array together dot_product = ([0] * [0]) + ([1] * [1])
 
-
+''' SINGLE NEURON 
 inputs = [1, 2, 3, 2.5] # imputs from 4 previous neuron
 weights = [[0.2, 0.8, -0.5, 1], 
            [0.5, -0.91, 0.26, -0.5], 
@@ -28,17 +27,42 @@ bias = [2, 3, 0.5]
 output = np.dot(weights, inputs) + bias 
 # for a single nueron, the first parameter (weights) is how many indexes should be returned
 print(output)
-
-
-
-
 '''
-layer_outputs = []
-for neuron_weights, neuron_bias in zip(weights, baises):
-    neuron_output = 0 
-    for n_input, weight in zip(inputs, neuron_weights):
-       neuron_output += n_input*weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
-print(layer_outputs)
-'''
+# ADDING MATRICES (times two values together, then add them) 
+# first row + first column    X 0 0 0 
+
+# first row + second column   0 X 0 0
+
+# second row + first column   O O O O 
+#                             X 0 0 0
+# After, add the bais to the output for each line
+
+# TRANSPOSING, switch the rows and columns of two matrices 
+
+# ACTIVATION FUNCTION
+# after the calculation with the bais.
+# Step Function, if the input is <= 0 then it's 0, if > 0 then it's 1
+# Sigmoid Functiom, 0 to 1, x0y0 is 0.5
+# Rectified Linear function, only if x > 0 will it output x
+
+# inputs = x 
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]] # iNputs from 4 previous neuron
+
+class Layer_Dense:
+     def __init__(self, n_inputs , n_neurons): # the shape of the matrix 
+          self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+          self.biases = np.zeros((1, n_neurons))
+     def forward(self, inputs):
+          self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = Layer_Dense(4, 5) # second param must be the size of th efirst
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
+
+# 
