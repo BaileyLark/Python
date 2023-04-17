edac2529dd5f2a3b2421de34ab7f3040951baa15
BaileyLark <bailey.lark1@gmail.com>
2023-04-17 20:14:12 +0100

    0.24

--- Mar-23/Tkinter.py
+++ Mar-23/Tkinter.py
@@ -1,17 +1,19 @@
 import tkinter as tk 
 from tkinter import messagebox
 from PIL import ImageTk, Image
+from tkinter import filedialog
 
 root = tk.Tk()
+root.geometry("500x500")
 
 # Frames
 frame = tk.LabelFrame(root, text="Frame Name", padx=10, pady=10)
-
+#my_image = tk.ImageTK.PhotoImage(Image.open())
 
 # Selection boxes
 r = tk.IntVar()
-tk.Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda:clicked(1))
-tk.Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda:clicked(2))
+tk.Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda:...)
+tk.Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda:...)
 
 # Message Boxes
 
@@ -20,9 +22,24 @@ def popup():
     response = messagebox.askquestion("Title", "Message")
 tk.Button(root, text="Click Me", command=popup)
 
-# Creating new window 
+# Opening a file selection box
+#root.filename = filedialog.askopenfilename(initialdir="C:/downloads", title="Select a File")
 
-top = tk.Toplevel()
+vertical = tk.Scale(root, from_=0, to=100, orient="horizontal")
+vertical.pack()
 
+# checkboxes 
+var = tk.IntVar()
+c1 = tk.Checkbutton(root, text="Check This Box", variable=var)
+
+# Drop down boxes 
+clicked = tk.StringVar()
+options = [
+    "item0",
+    "item1",
+    "item2",
+]
+clicked.set("item0")
+drop = tk.OptionMenu(root, clicked, *options).pack()
 
 root.mainloop()
