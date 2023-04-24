c6c9b6e5a295d3f6720b34b5cd76feadaa2dd02d
BaileyLark <bailey.lark1@gmail.com>
2023-04-24 23:02:04 +0100

    0.27

--- Mar-23/Tkinter.py
+++ Mar-23/Tkinter.py
@@ -9,9 +9,11 @@ root = tk.Tk()
 def click(event):
     myLabel = tk.Label(root, text="you clicked a button").pack()
 
+my_menu = tk.Menu(root)
+root.config(menu=my_menu)
 
-myButton = tk.Button(root, text="Click Me", command=click)
-myButton.bind("<Button-3>", click)
-myButton.pack()
+file_menu = tk.Menu(my_menu)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New...", command=...)
 
 root.mainloop()
