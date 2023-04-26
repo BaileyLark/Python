95c45ee3b2adb6d079e7a3e8ef8da9be0502cbfd
BaileyLark <bailey.lark1@gmail.com>
2023-04-26 20:38:20 +0100

    0.28

--- Mar-23/Tkinter.py
+++ Mar-23/Tkinter.py
@@ -6,14 +6,7 @@ import sqlite3
 
 root = tk.Tk()
 
-def click(event):
-    myLabel = tk.Label(root, text="you clicked a button").pack()
+my 
 
-my_menu = tk.Menu(root)
-root.config(menu=my_menu)
-
-file_menu = tk.Menu(my_menu)
-my_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New...", command=...)
 
 root.mainloop()
