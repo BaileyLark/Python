3f839c0db6001f7b5682853883f121ab9877d240
BaileyLark <bailey.lark1@gmail.com>
2023-05-15 22:49:16 +0100

    0.33

--- May-23/C++/C++/Source.cpp
+++ May-23/C++/C++/Source.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
 #include <string>
 
-class Planet
-{
-
+int main() {
 
 }
 
-
-
-int main()
-{
-
-	int hello{ 15 };
-	int* hello2{ &hello };
-
-	std::cout << hello << hello2 << std::endl;
-
-};
+// 
