0e417da2ab4b480f4e807ba7f0c80b0ce4f7f6ed
BaileyLark <bailey.lark1@gmail.com>
2023-05-21 20:36:48 +0100

    0.35

--- May-23/C++/C++/Source.cpp
+++ May-23/C++/C++/Source.cpp
@@ -2,11 +2,20 @@
 #include <string>
 #include "Classes.h"
 
+void printer() { std::cout << "working"; }
+
 int main() {
-	Health_Component testing = Health_Component(5, 5);
-	std::cout << testing.getHealth() << std::endl;
-	testing.setHealth(2);
-	std::cout << testing.getHealth() << std::endl;
+
+	Stats_Component statsTesting = Stats_Component(4, 5, 3);
+	std::cout << statsTesting.getStats()[0] << "\n"; 
 }
 
-// 
+
+
+
+// unanmed namescape, contents can only be accessed inside file
+// inline namespace, the default (if multiple of the same name exist)
+
+// function pointer
+//void (*ptr)() = &printer; // has to be in brackets otherwise it calls the function void* ptr()
+//(*ptr)(); // dereference then pass the value through
